39249aded26afcf22f0162ab4cf5a23e53d9bec8
Panda <juanck224@gmail.com>
2020-08-18 21:27:44 -0400

    Added for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,23 @@
 
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+//    * Fight all enemy robots
+//    * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
+
+//Player Initial Values
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+//Enemy Initial Values
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+var fight = function(enemyName) {
+
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -66,4 +72,7 @@ var fight = function () {
 
 };
 
-fight();
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
