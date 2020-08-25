e7b6b7e9e716230c87e2b85c134ae519fce1b15c
Panda <juanck224@gmail.com>
2020-08-24 23:29:52 -0400

    Bug splat fix null name

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,7 +15,7 @@ var fight = function(enemy) {
     var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
     // if user picks "skip" confirm and then stop the loop
-    if (promptFight === "skip" || promptFight === "SKIP") {
+    if (promptFight.toLowerCase() === "skip" || promptFight === "SKIP") {
       // confirm user wants to skip
       var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -82,6 +82,7 @@ var startGame = function () {
     if (playerInfo.health > 0) {
       // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+   
 
       // pick new enemy to fight based on the index of the enemyNames array
       var pickedEnemyObj = enemyInfo[i];
@@ -172,9 +173,23 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+// ***************************************
+while (name === "" || name === null) {
+  name = prompt("What is your robot's name?");
+}
+// ***************************************
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 //Player Initial Values
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
